daa0bec02fff9d77b9d004028a1feb465ef8db4c
Dongbin Na <ndb796@naver.com>
2020-04-16 08:37:09 +0900

    Create 11.py

---
+++ 5/11.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+# N, M을 공백을 기준으로 구분하여 입력 받기
+n, m = map(int, input().split())
+# 2차원 리스트의 맵 정보 입력 받기
+array = []
+for i in range(n):
+    array.append(list(map(int, input())))
+
+# 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# BFS 소스코드 구현
+def bfs(x, y):
+    # 큐(Queue) 구현을 위해 dequue 라이브러리 사용
+    queue = deque()
+    queue.append((x, y))
+    # 큐가 빌 때까지 반복하기
+    while queue:
+        x, y = queue.popleft()
+        # 현재 위치에서 4가지 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 미로 찾기 공간을 벗어난 경우 무시
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            # 벽인 경우 무시
+            if array[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if array[nx][ny] == 1:
+                array[nx][ny] = array[x][y] + 1
+                queue.append((nx, ny))
+    # 가장 오른쪽 아래까지의 최단 거리 반환
+    return array[n - 1][m - 1]
+
+# BFS를 수행한 결과 출력
+print(bfs(0, 0))
