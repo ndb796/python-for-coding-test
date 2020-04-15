b512e9468abb84fd0d053efe26b84d085795b473
Dongbin Na <ndb796@naver.com>
2020-04-16 07:07:43 +0900

    Create 2.py

---
+++ 3/2.py
@@ -0,0 +1,18 @@
+# n, m, k를 공백을 기준으로 구분하여 입력 받기
+n, m, k = map(int, input().split())
+# n개의 수를 공백을 기준으로 구분하여 입력 받기
+data = list(map(int, input().split()))
+
+data.sort() # 입력 받은 수들 정렬하기
+first = data[n - 1] # 가장 큰 수
+second = data[n - 2] # 두 번째로 큰 수
+
+# 가장 큰 수가 더해지는 횟수 계산
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result = 0
+result += (count) * first # 가장 큰 수 더하기
+result += (m - count) * second # 두 번째로 큰 수 더하기
+
+print(result) # 최종 답안 출력
