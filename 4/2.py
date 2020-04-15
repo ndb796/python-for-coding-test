8fc61789f4627290530d173620a211de71a69e1f
Dongbin Na <ndb796@naver.com>
2020-04-16 07:34:42 +0900

    Create 2.py

---
+++ 4/2.py
@@ -0,0 +1,12 @@
+# H 입력 받기
+h = int(input())
+
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
