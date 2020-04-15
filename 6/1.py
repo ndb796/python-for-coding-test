9999277c2452db24a5ad3f17d6a53f517d38e13e
Dongbin Na <ndb796@naver.com>
2020-04-16 08:38:43 +0900

    Create 1.py

---
+++ 6/1.py
@@ -0,0 +1,10 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_index = i # 가장 작은 원소의 인덱스
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i] # 스와프
+
+print(array)
