16814669cbf62f6192967fb7c03d9958b5a4b81b
Dongbin Na <ndb796@naver.com>
2020-04-16 22:06:47 +0900

    Create 5.py

---
+++ 8/5.py
@@ -0,0 +1,16 @@
+# 정수 N을 입력 받기
+n = int(input())
+# 모든 식량 정보 입력 받기
+array = list(map(int, input().split()))
+
+# 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 리스트 초기화
+d = [0] * 100
+
+# 다이나믹 프로그래밍(Dynamic Programming) 진행 (Bottom-top)
+d[0] = array[0]
+d[1] = max(array[0], array[1]) 
+for i in range(2, n):
+    d[i] = max(d[i - 1], d[i - 2] + array[i])
+
+# 계산된 결과 출력
+print(d[n - 1])
