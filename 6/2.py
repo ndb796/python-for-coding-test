802574879f1aeba94217db78babec94af71b3628
Dongbin Na <ndb796@naver.com>
2020-04-17 08:50:27 +0900

    Update 2.py

--- 6/2.py
+++ 6/2.py
@@ -4,7 +4,7 @@ for i in range(1, len(array)):
     for j in range(i, 0, -1): # 인덱스 i부터 1까지 감소하며 반복하는 문법
         if array[j] < array[j - 1]: # 한 칸씩 왼쪽으로 이동
             array[j], array[j - 1] = array[j - 1], array[j]
-        else: # 자기보다 작은 데이터를 만나면 그 위치에 멈춤
+        else: # 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
             break
 
 print(array)
