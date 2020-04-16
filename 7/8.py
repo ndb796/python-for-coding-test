acbf9e8ca7895dea76c42a152a06f18c07d90a91
Dongbin Na <ndb796@naver.com>
2020-04-16 21:49:43 +0900

    Create 8.py

---
+++ 7/8.py
@@ -0,0 +1,28 @@
+# 떡의 개수(N)와 요청한 떡의 길이(M)을 입력
+n, m = list(map(int, input().split(' ')))
+# 각 떡의 개별 높이 정보를 입력
+array = list(map(int, input().split()))
+
+# 이진 탐색을 위한 시작점과 끝점 설정
+start = 0
+end = max(array)
+
+# 이진 탐색 수행 (반복적)
+result = 0
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for i in array:
+        # 잘랐을 때의 떡볶이 양 계산
+        if i > mid:
+            total += i - mid
+    # 떡볶이 양이 부족한 경우 더 많이 자르기
+    if total < m:
+        end = mid - 1
+    # 떡볶이 양이 충분한 경우 덜 자르기
+    else:
+        result = mid
+        start = mid + 1
+
+# 정답 출력
+print(result)
