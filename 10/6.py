60cee4781015bbed08521705d70393da7908f044
Dongbin Na <ndb796@naver.com>
2020-04-21 12:11:58 +0900

    Create 6.py

---
+++ 10/6.py
@@ -0,0 +1,35 @@
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+parent = {}
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(0, n + 1):
+    parent[i] = i
+
+# 각 연산을 하나씩 확인한다.
+for i in range(m):
+    oper, a, b = map(int, input().split())
+    # 합치기(Union) 연산인 경우
+    if oper == 0:
+        union_parent(parent, a, b)
+    # 찾기(Find) 연산인 경우
+    elif oper == 1:
+        if find_parent(parent, a) == find_parent(parent, b):
+            print('YES')
+        else:
+            print('NO')
