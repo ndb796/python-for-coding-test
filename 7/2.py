5364c46164671977abd1ce2af0105f78fe3c5a20
Dongbin Na <ndb796@naver.com>
2020-06-10 23:48:39 +0900

    Update 2.py

--- 7/2.py
+++ 7/2.py
@@ -1,14 +1,17 @@
 # 이진 탐색 소스코드 구현 (재귀 함수)
-def binary_search(start, end, target, array):
+def binary_search(array, target, start, end):
     if start > end:
         return None
     mid = (start + end) // 2
+    # 찾은 경우 중간점 인덱스 반환
     if array[mid] == target:
         return mid
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
     elif array[mid] > target:
-        return binary_search(start, mid - 1, target, array)
+        return binary_search(array, target, start, mid - 1)
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 오른쪽 확인
     else:
-        return binary_search(mid + 1, end, target, array)
+        return binary_search(array, target, mid + 1, end)
 
 # n(원소의 개수)과 target(찾고자 하는 문자열)을 입력 받기
 n, target = list(map(int, input().split()))
@@ -16,7 +19,7 @@ n, target = list(map(int, input().split()))
 array = list(map(int, input().split()))
 
 # 이진 탐색 수행 결과 출력
-result = binary_search(0, n - 1, target, array)
+result = binary_search(array, target, 0, n - 1)
 if result == None:
     print(None)
 else:
