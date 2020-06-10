09a4503c1e780c611ce985367906a657382ff99c
Dongbin Na <ndb796@naver.com>
2020-06-10 22:14:16 +0900

    Update 3.py

--- 4/3.py
+++ 4/3.py
@@ -12,6 +12,7 @@ steps = [
 # 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
 result = 0
 for step in steps:
+    # 이동하고자 하는 위치 확인
     next_row = row + step[0]
     next_column = column + step[1]
     # 해당 위치로 이동이 가능하다면 카운트 증가
