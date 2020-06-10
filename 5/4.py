a93111484df058dc4b553e7bb750f2de3b3de2c5
Dongbin Na <ndb796@naver.com>
2020-06-10 17:10:48 +0900

    Update 4.py

--- 5/4.py
+++ 5/4.py
@@ -1,8 +1,9 @@
 def recursive_function(i):
-    # 100번째 출력을 했을 때 종료되도록 종료 조건 명시
+    # 100번째 호출을 했을 때 종료되도록 종료 조건 명시
     if i == 100:
         return
-    print('재귀 함수를 호출합니다.')
+    print(i, '번째 재귀함수에서', i + 1, '번째 재귀함수를 호출합니다.')
     recursive_function(i + 1)
+    print(i, '번째 재귀함수를 종료합니다.')
 
-recursive_function(0)
+recursive_function(1)
