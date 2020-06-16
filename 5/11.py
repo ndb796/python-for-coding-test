8943f6ead4ca60b0201fd3d49353821dec7591e5
Dongbin Na <ndb796@naver.com>
2020-06-17 04:52:58 +0900

    Update 11.py

--- 5/11.py
+++ 5/11.py
@@ -3,9 +3,9 @@ from collections import deque
 # N, M을 공백을 기준으로 구분하여 입력 받기
 n, m = map(int, input().split())
 # 2차원 리스트의 맵 정보 입력 받기
-array = []
+graph = []
 for i in range(n):
-    array.append(list(map(int, input())))
+    graph.append(list(map(int, input())))
 
 # 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
 dx = [-1, 1, 0, 0]
@@ -24,17 +24,17 @@ def bfs(x, y):
             nx = x + dx[i]
             ny = y + dy[i]
             # 미로 찾기 공간을 벗어난 경우 무시
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
             # 벽인 경우 무시
-            if array[nx][ny] == 0:
+            if graph[nx][ny] == 0:
                 continue
             # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-            if array[nx][ny] == 1:
-                array[nx][ny] = array[x][y] + 1
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
     # 가장 오른쪽 아래까지의 최단 거리 반환
-    return array[n - 1][m - 1]
+    return graph[n - 1][m - 1]
 
 # BFS를 수행한 결과 출력
 print(bfs(0, 0))
