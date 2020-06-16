ac8fc51f1bb99950baf5776c7a0cb375dd6e55a8
Dongbin Na <ndb796@naver.com>
2020-06-17 03:38:41 +0900

    Update 8.py

--- 5/8.py
+++ 5/8.py
@@ -1,26 +1,28 @@
-# DFS 함수 정의
-def dfs(adj, v, visited):
+# DFS 메서드 정의
+def dfs(graph, v, visited):
     # 현재 노드를 방문 처리
-    visited.add(v)
+    visited[v] = True
     print(v, end=' ')
     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-    for i in adj[v]:
-        if i not in visited:
-            dfs(adj, i, visited)
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-# 각 노드가 연결된 정보를 dict 자료형으로 표현 (2차원 리스트 대신)
-adj = {
-  1: [2, 3, 8],
-  2: [1, 7],
-  3: [1, 4, 5],
-  4: [3, 5],
-  5: [3, 4],
-  6: [7],
-  7: [2, 6, 8],
-  8: [1, 7]
-}
-# 각 노드가 방문된 정보를 set 자료형으로 표현 (1차원 리스트 대신)
-visited = set()
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현 (2차원 리스트)
+graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7]
+]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현 (1차원 리스트)
+visited = [False] * 9
 
 # 정의된 DFS 함수 호출
-dfs(adj, 1, visited)
+dfs(graph, 1, visited)
