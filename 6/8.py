ccd4d5844e6787f88e22c3a20d9c2ec502fcaefa
Dongbin Na <ndb796@naver.com>
2020-06-17 05:42:41 +0900

    Update 8.py

--- 6/8.py
+++ 6/8.py
@@ -4,7 +4,9 @@ n = int(input())
 # N명의 학생 정보를 입력 받아 리스트에 저장
 array = []
 for i in range(n):
-    array.append(input().split())
+    input_data = input().split()
+    # 이름은 문자열 그대로, 점수는 정수형으로 변환하여 저장
+    array.append((input_data[0], int(input_data[1])))
 
 # 키(Key)를 이용하여, 점수를 기준으로 정렬
 array = sorted(array, key=lambda student: student[1])
