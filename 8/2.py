23c167acc1d570debe58c29290822bb76ee129c5
Dongbin Na <ndb796@naver.com>
2020-06-17 08:22:46 +0900

    Update 2.py

--- 8/2.py
+++ 8/2.py
@@ -1,7 +1,7 @@
 # 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 리스트 초기화
 d = [0] * 100
 
-# 피보나치 함수(Fibonacci Function)를 재귀함수로 구현 (다이나믹 프로그래밍)
+# 피보나치 함수(Fibonacci Function)를 재귀함수로 구현 (탑다운 다이나믹 프로그래밍)
 def fibo(x):
     # 종료 조건(1 혹은 2일 때 1을 반환)
     if x == 1 or x == 2:
