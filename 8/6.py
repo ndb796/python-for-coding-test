70f57a2b72e7171cd43bc55c1ee2d5aa5eca7c6d
Dongbin Na <ndb796@naver.com>
2020-06-17 08:28:33 +0900

    Update 6.py

--- 8/6.py
+++ 8/6.py
@@ -1,9 +1,10 @@
 # 정수 N을 입력 받기
 n = int(input())
-# 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 리스트 초기화
+
+# 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
 d = [0] * 1000001
 
-# 다이나믹 프로그래밍(Dynamic Programming) 진행 (Bottom-top)
+# 다이나믹 프로그래밍(Dynamic Programming) 진행 (보텀업)
 d[1] = 1
 d[2] = 3
 for i in range(3, n + 1):
