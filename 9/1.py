9a264528d321ed54ae8f62a1a652cc9893cead94
Dongbin Na <ndb796@naver.com>
2020-06-18 07:31:05 +0900

    Update 1.py

--- 9/1.py
+++ 9/1.py
@@ -7,7 +7,7 @@ n, m = map(int, input().split())
 # 시작 노드 번호를 입력 받습니다.
 start = int(input())
 # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만듭니다.
-graph = [[] for i in range (n + 1)]
+graph = [[] for i in range(n + 1)]
 # 방문한 적이 있는지 체크하는 목적의 리스트를 만듭니다.
 visited = [False] * (n + 1)
 # 최단 거리 테이블을 모두 무한으로 초기화합니다.
