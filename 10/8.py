eadd75ed4b1ba7c45d43dd5c1a887e16579b2f99
Dongbin Na <ndb796@naver.com>
2020-06-18 08:49:32 +0900

    Update 8.py

--- 10/8.py
+++ 10/8.py
@@ -5,8 +5,8 @@ import copy
 v = int(input())
 # 모든 노드에 대한 진입차수는 0으로 초기화
 indegree = [0] * (v + 1)
-# 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트 초기화
-adj = [[] for i in range(v + 1)]
+# 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트(그래프) 초기화
+graph = [[] for i in range(v + 1)]
 # 각 건물이 지어지는 시간을 0으로 초기화
 time = [0] * (v + 1)
 
@@ -16,12 +16,13 @@ for i in range(1, v + 1):
     time[i] = data[0] # 첫 번째 수는 시간 정보를 담고 있음
     for x in data[1:-1]:
         indegree[i] += 1
-        adj[x].append(i)
+        graph[x].append(i)
 
 # 위상 정렬 함수
 def topology_sort():
     result = copy.deepcopy(time) # 알고리즘 수행 결과를 담을 리스트
     q = deque() # 큐 기능을 위한 deque 라이브러리 사용
+
     # 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
     for i in range(1, v + 1):
         if indegree[i] == 0:
@@ -32,7 +33,7 @@ def topology_sort():
         # 큐에서 원소 꺼내기
         now = q.popleft()
         # 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
-        for i in adj[now]:
+        for i in graph[now]:
             result[i] = max(result[i], result[now] + time[i])
             indegree[i] -= 1
             # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
