72b2351fe7320244cda9a3902f43f8b28bba0737
Dongbin Na <ndb796@naver.com>
2020-06-18 06:23:18 +0900

    Update 10.py

--- 5/10.py
+++ 5/10.py
@@ -10,7 +10,7 @@ for i in range(n):
 def dfs(x, y):
     # 주어진 범위를 벗어나는 경우에는 즉시 종료
     if x <= -1 or x >= n or y <= -1 or y >= m:
-        return 0
+        return False
     # 현재 노드를 아직 방문하지 않았다면
     if graph[x][y] == 0:
         # 해당 노드 방문 처리
@@ -20,15 +20,15 @@ def dfs(x, y):
         dfs(x, y - 1)
         dfs(x + 1, y)
         dfs(x, y + 1)
-        return 1
-    return 0
+        return True
+    return False
 
 # 모든 노드(위치)에 대하여 음료수 채우기
 result = 0
 for i in range(n):
     for j in range(m):
         # 현재 위치에서 DFS 수행
-        if dfs(i, j) == 1:
+        if dfs(i, j) == True:
             result += 1
 
 print(result) # 정답 출력
