b1bc8979098ed1ce4cfc29ccb192360e9d656000
Dongbin Na <ndb796@naver.com>
2020-06-18 07:06:18 +0900

    Update 7.py

--- 8/7.py
+++ 8/7.py
@@ -5,10 +5,10 @@ array = []
 for i in range(n):
     array.append(int(input()))
 
-# 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 리스트 초기화
+# 한 번 계산된 결과를 저장하기 위한 DP 테이블 초기화
 d = [10001] * (m + 1)
 
-# 다이나믹 프로그래밍(Dynamic Programming) 진행 (Bottom-top)
+# 다이나믹 프로그래밍(Dynamic Programming) 진행 (보텀업)
 d[0] = 0
 for i in range(n):
     for j in range(array[i], m + 1):
