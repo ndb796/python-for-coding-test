8c89847f3f81afc40eec73c43e4547a15b6acd9c
Dongbin Na <ndb796@naver.com>
2020-06-18 08:32:34 +0900

    Update 3.py

--- 10/3.py
+++ 10/3.py
@@ -2,7 +2,7 @@
 def find_parent(parent, x):
     # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
     if parent[x] != x:
-       parent[x] = find_parent(parent, parent[x])
+        parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
 # 두 원소가 속한 집합을 합치기
