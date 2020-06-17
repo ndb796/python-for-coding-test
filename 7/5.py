5ff8f832e8e690eef4c6c925ea2e0b5dff919292
Dongbin Na <ndb796@naver.com>
2020-06-18 06:44:54 +0900

    Update 5.py

--- 7/5.py
+++ 7/5.py
@@ -1,14 +1,17 @@
-# 이진 탐색 소스코드 구현 (재귀 함수)
-def search(start, end, target, array):
-    if start > end:
-        return None
-    mid = (start + end) // 2
-    if array[mid] == target:
-        return mid
-    elif array[mid] > target:
-        return search(start, mid - 1, target, array)
-    else:
-        return search(mid + 1, end, target, array)
+# 이진 탐색 소스코드 구현 (반복문)
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        # 찾은 경우 중간점 인덱스 반환
+        if array[mid] == target:
+            return mid
+        # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+        elif array[mid] > target:
+            end = mid - 1
+        # 중간점의 값보다 찾고자 하는 값이 작은 경우 오른쪽 확인
+        else:
+            start = mid + 1
+    return None
 
 # N(가게의 부품 개수) 입력
 n = int(input())
@@ -23,7 +26,7 @@ x = list(map(int, input().split()))
 # 손님이 확인 요청한 부품 번호를 하나씩 확인
 for i in x:
     # 해당 부품이 존재하는지 확인
-    result = search(0, n - 1, i, array)
+    result = binary_search(array, i, 0, n - 1)
     if result != None:
         print('yes', end=' ')
     else:
