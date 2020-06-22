5f70283d169bfcb214372800216161b7e756a620
Dongbin Na <ndb796@naver.com>
2020-06-22 17:25:02 +0900

    Create 1.py

---
+++ 11/1.py
@@ -0,0 +1,14 @@
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+result = 0 # 가능한 그룹의 수
+count = 0 # 그룹에 포함된 모험가의 수
+
+for i in data: # 공포도를 낮은 것부터 하나씩 확인하며
+    count += 1 # 그룹에 해당 모험가를 포함시키기
+    if count >= i: # 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성하여 여행 보내기
+        result += 1 # 그룹의 수 증가시키기
+        count = 0 # 그룹에 포함된 모험가의 수 초기화
+
+print(result) # 그룹의 수 출력
