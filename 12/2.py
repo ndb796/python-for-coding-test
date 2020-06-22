8b05e1733c1762365a17dc327e90b3b10f6d3cfe
Dongbin Na <ndb796@naver.com>
2020-06-23 03:51:09 +0900

    Create 2.py

---
+++ 12/2.py
@@ -0,0 +1,22 @@
+data = input()
+result = []
+value = 0
+
+# 문자를 하나씩 확인하며
+for x in data:
+    # 알파벳인 경우 결과 리스트에 삽입
+    if x.isalpha():
+        result.append(x)
+    # 숫자는 따로 더하기
+    else:
+        value += int(x)
+
+# 알파벳을 오름차순으로 정렬
+result.sort()
+
+# 숫자가 하나라도 존재하는 경우 가장 뒤에 삽입
+if value != 0:
+    result.append(str(value))
+
+# 최종 결과 출력 (리스트를 문자열로 변환하여 출력)
+print(''.join(result))
