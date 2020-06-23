459876424c9f9c4a71c0f57d2e181d8e525d8fa9
Dongbin Na <ndb796@naver.com>
2020-06-23 17:03:33 +0900

    Create 5.py

---
+++ 13/5.py
@@ -0,0 +1,36 @@
+n = int(input())
+# 연산을 수행하고자 하는 수 리스트
+data = list(map(int, input().split()))
+# 더하기, 빼기, 곱하기, 나누기 연산자 개수
+add, sub, mul, div = map(int, input().split())
+
+min_value = 1e9
+max_value = -1e9
+
+def dfs(i, now):
+    global min_value, max_value, add, sub, mul, div
+    if i == n:
+        min_value = min(min_value, now)
+        max_value = max(max_value, now)
+    else:
+        # 각 연산자에 대하여 DFS 수행
+        if add > 0:
+            add -= 1
+            dfs(i + 1, now + data[i])
+            add += 1
+        if sub > 0:
+            sub -= 1
+            dfs(i + 1, now - data[i])
+            sub += 1
+        if mul > 0:
+            mul -= 1
+            dfs(i + 1, now * data[i])
+            mul += 1
+        if div > 0:
+            div -= 1
+            dfs(i + 1, int(now / data[i])) # 나눌 때는 나머지를 제거
+            div += 1
+
+dfs(1, data[0])
+print(max_value)
+print(min_value)
