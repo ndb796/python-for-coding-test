63981ee26a0b659d3ef3bdcb0f6a974c44bc615a
Dongbin Na <ndb796@naver.com>
2020-06-23 16:58:23 +0900

    Create 3.py

---
+++ 13/3.py
@@ -0,0 +1,43 @@
+from collections import deque
+n, k = map(int, input().split())
+
+# N x N 크기의 보드 전체를 0으로 초기화
+board = []
+data = [] 
+
+for i in range(n):
+    # 보드 정보를 한 줄 단위로 입력
+    board.append(list(map(int, input().split())))
+    for j in range(n):
+        # 해당 위치에 바이러스가 존재하는 경우
+        if board[i][j] != 0:
+            # (바이러스 종류, 시간, 위치 X, 위치 Y) 삽입
+            data.append((board[i][j], 0, i, j))
+
+# 정렬 이후에 큐로 옮기기
+data.sort()
+q = deque(data)
+ 
+target_s, target_x, target_y = map(int, input().split())
+ 
+# 바이러스가 퍼져나갈 수 있는 4가지의 위치
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+ 
+while q:
+    virus, s, x, y = q.popleft()
+    # 정확히 s초가 지나거나, 큐가 빌 때까지 반복
+    if s == target_s:
+        break
+    # 4가지 위치를 각각 확인
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        # 해당 위치로 이동할 수 있는 경우
+        if 0 <= nx and nx < n and 0 <= ny and ny < n:
+            # 방문한 적 없다면, 그 위치에 바이러스 넣기
+            if board[nx][ny] == 0:
+                board[nx][ny] = virus
+                q.append((virus, s + 1, nx, ny))
+
+print(board[target_x - 1][target_y - 1])
