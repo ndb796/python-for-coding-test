82e14aa8e2df246991a31f2d94d0efb8953f8ef7
Dongbin Na <ndb796@naver.com>
2020-06-23 16:48:10 +0900

    Create 1.py

---
+++ 13/1.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+# 도시의 개수, 도로의 개수, 거리 정보, 출발 도시 번호
+n, m, k, x = map(int, input().split())
+adj = [[] for _ in range(n + 1)]
+
+# 모든 도로 정보 입력 받기
+for _ in range(m):
+    a, b = map(int, input().split())
+    adj[a].append(b)
+
+# 모든 노드에 대한 최단 거리 초기화
+distance = [-1] * (n + 1)
+distance[x] = 0 # 출발 도시까지의 거리는 0으로 설정
+
+# BFS 수행
+q = deque([x])
+while q:
+    now = q.popleft()
+    # 이동할 수 있는 모든 도시를 확인
+    for next_node in adj[now]:
+        # 아직 방문하지 않은 도시라면
+        if distance[next_node] == -1:
+            # 최단 거리 갱신
+            distance[next_node] = distance[now] + 1
+            q.append(next_node)
+
+# 최단 거리가 K인 모든 도시의 번호를 오름차순으로 출력
+check = False
+for i in range(1, n + 1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+# 만약 최단 거리가 K인 도시가 없다면, -1 출력
+if check == False:
+    print(-1)
