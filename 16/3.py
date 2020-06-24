fe8da4e07cab32418d2d1ef07a6df77433958b1d
Dongbin Na <ndb796@naver.com>
2020-06-24 11:13:28 +0900

    Create 3.py

---
+++ 16/3.py
@@ -0,0 +1,24 @@
+n = int(input())
+t = []
+p = []
+dp = [0] * (n + 1)
+max_value = 0
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    t.append(x)
+    p.append(y)
+
+# 리스트를 뒤에서부터 거꾸로 확인
+for i in range(n - 1, -1, -1):
+    time = t[i] + i
+    # 상담이 기간 안에 끝나는 경우
+    if time <= n:
+        # 점화식에 맞게, 현재까지의 최고 이익 계산
+        dp[i] = max(p[i] + dp[time], max_value)
+        max_value = dp[i]
+    # 상담이 기간을 벗어나는 경우
+    else:
+        dp[i] = max_value
+
+print(max(dp))
