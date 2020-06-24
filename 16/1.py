d02ccd3c293233731188ebcbcc543c0fb5618b46
Dongbin Na <ndb796@naver.com>
2020-06-24 11:10:04 +0900

    Update 1.py

--- 16/1.py
+++ 16/1.py
@@ -4,7 +4,7 @@ for tc in range(int(input())):
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
 
-    # 다이나믹 프로그래밍을 위한 이차원 테이블 초기화
+    # 다이나믹 프로그래밍을 위한 2차원 DP 테이블 초기화
     dp = []
     index = 0
     for i in range(n):
@@ -14,19 +14,19 @@ for tc in range(int(input())):
     # 다이나믹 프로그래밍 진행
     for j in range(1, m):
         for i in range(n):
-        # 왼쪽 위에서 오는 경우
-        if i = = 0:
-            left_up = 0
-        else:
-            left_up = dp[i - 1][j - 1]
-        # 왼쪽 아래에서 오는 경우
-        if i = = n - 1:
-            left_down = 0
-        else:
-            left_down = dp[i + 1][j - 1]
-        # 왼쪽에서 오는 경우
-        left = dp[i][j - 1]
-        dp[i][j] = dp[i][j] + max(left_up, left_down, left)
+            # 왼쪽 위에서 오는 경우
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i - 1][j - 1]
+            # 왼쪽 아래에서 오는 경우
+            if i == n - 1:
+                left_down = 0
+            else:
+                left_down = dp[i + 1][j - 1]
+            # 왼쪽에서 오는 경우
+            left = dp[i][j - 1]
+            dp[i][j] = dp[i][j] + max(left_up, left_down, left)
 
     result = 0
     for i in range(n):
