520f5fde6387f2f2a64566d9752ac91da2022687
Dongbin Na <ndb796@naver.com>
2020-06-29 07:51:21 +0900

    Update 4.py

--- 14/4.py
+++ 14/4.py
@@ -1,14 +1,23 @@
 import heapq
+
 n = int(input())
+
+# 힙(Heap) 자료구조에 초기 카드 묶음을 삽입
 heap = []
 for i in range(n):
     data = int(input())
     heapq.heappush(heap, data)
+
 result = 0
+
+# 힙(Heap)에 원소가 1개 남을 때까지
 while len(heap) != 1:
+    # 가장 작은 2개의 카드 묶음 꺼내기
     one = heapq.heappop(heap)
     two = heapq.heappop(heap)
+    # 카드 묶음을 합쳐서 다시 삽입
     sum_value = one + two
     result += sum_value
     heapq.heappush(heap, sum_value)
+
 print(result)
