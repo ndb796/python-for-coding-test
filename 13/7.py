9f8c64baa9e9e08e18b9038a2bf8a27e3c0ef531
Dongbin Na <ndb796@naver.com>
2020-06-29 07:04:21 +0900

    Update 7.py

--- 13/7.py
+++ 13/7.py
@@ -1,10 +1,12 @@
 from collections import deque
 
+# 땅의 크기(N), L, R 값을 입력 받기
 n, l, r = map(int, input().split())
-data = []
 
+# 전체 나라의 정보(N x N)를 입력 받기
+graph = []
 for _ in range(n):
-    data.append(list(map(int, input().split())))
+    graph.append(list(map(int, input().split())))
 
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
@@ -13,31 +15,35 @@ result = 0
 
 # 특정 위치에서 출발하여 모든 연합을 체크한 뒤에 데이터 갱신
 def process(x, y, index):
-    routes = []
-    routes.append((x, y))
-    # DFS를 위한 큐 자료구조 정의
+    # (x, y)의 위치와 연결된 나라(연합) 정보를 담는 리스트
+    united = []
+    united.append((x, y))
+    # 너비 우선 탐색 (BFS)을 위한 큐 자료구조 정의
     q = deque()
     q.append((x, y))
-    union[x][y] = index
-    summary = data[x][y]
-    count = 1
+    union[x][y] = index # 현재 연합의 번호 할당
+    summary = graph[x][y] # 현재 연합의 전체 인구 수
+    count = 1 # 현재 연합의 국가 수
     # 큐가 빌 때까지 반복(BFS)
     while q:
         x, y = q.popleft()
+        # 현재 위치에서 4가지 방향을 확인하며
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            # 바로 옆 나라와 국경선이 열린 경우
+            # 바로 옆에 있는 나라를 확인하여
             if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
-                if l <= abs(data[nx][ny] - data[x][y]) <= r:
-                    union[nx][ny] = index
-                    summary += data[nx][ny]
-                    count += 1
+                # 옆에 있는 나라와 인구 차이가 L명 이상, R명 이하라면
+                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
                     q.append((nx, ny))
-                    routes.append((nx, ny))
+                    # 연합에 추가하기
+                    union[nx][ny] = index
+                    summary += graph[nx][ny]
+                    count += 1
+                    united.append((nx, ny))
     # 연합 국가끼리 인구를 분배
-    for i, j in routes:
-        data[i][j] = summary // count
+    for i, j in united:
+        graph[i][j] = summary // count
     return count
 
 total_count = 0
@@ -51,11 +57,10 @@ while True:
             if union[i][j] == -1: # 해당 나라가 아직 처리되지 않았다면
                 process(i, j, index)
                 index += 1
-
     # 모든 인구 이동이 끝난 경우
     if index == n * n:
         break
-
     total_count += 1
 
+# 인구 이동 횟수 출력
 print(total_count)
