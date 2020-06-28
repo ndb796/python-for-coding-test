dfa06d90533efb10457cccea5e7fda976235ea3c
Dongbin Na <ndb796@naver.com>
2020-06-29 06:07:09 +0900

    Update 5.py

--- 13/5.py
+++ 13/5.py
@@ -4,16 +4,19 @@ data = list(map(int, input().split()))
 # 더하기, 빼기, 곱하기, 나누기 연산자 개수
 add, sub, mul, div = map(int, input().split())
 
+# 최솟값과 최댓값 초기화
 min_value = 1e9
 max_value = -1e9
 
+# 깊이 우선 탐색 (DFS) 메서드
 def dfs(i, now):
     global min_value, max_value, add, sub, mul, div
+    # 모든 연산자를 다 사용한 경우, 최솟값과 최댓값 업데이트
     if i == n:
         min_value = min(min_value, now)
         max_value = max(max_value, now)
     else:
-        # 각 연산자에 대하여 DFS 수행
+        # 각 연산자에 대하여 재귀적으로 수행
         if add > 0:
             add -= 1
             dfs(i + 1, now + data[i])
@@ -31,6 +34,9 @@ def dfs(i, now):
             dfs(i + 1, int(now / data[i])) # 나눌 때는 나머지를 제거
             div += 1
 
+# DFS 메서드 호출
 dfs(1, data[0])
+
+# 최댓값과 최솟값 차례대로 출력
 print(max_value)
 print(min_value)
