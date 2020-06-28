4eb4ef5468446d17932643f241c385fdabe2189d
Dongbin Na <ndb796@naver.com>
2020-06-29 05:05:58 +0900

    Update 1.py

--- 13/1.py
+++ 13/1.py
@@ -2,23 +2,23 @@ from collections import deque
 
 # 도시의 개수, 도로의 개수, 거리 정보, 출발 도시 번호
 n, m, k, x = map(int, input().split())
-adj = [[] for _ in range(n + 1)]
+graph = [[] for _ in range(n + 1)]
 
 # 모든 도로 정보 입력 받기
 for _ in range(m):
     a, b = map(int, input().split())
-    adj[a].append(b)
+    graph[a].append(b)
 
-# 모든 노드에 대한 최단 거리 초기화
+# 모든 도시에 대한 최단 거리 초기화
 distance = [-1] * (n + 1)
 distance[x] = 0 # 출발 도시까지의 거리는 0으로 설정
 
-# BFS 수행
+# 너비 우선 탐색(BFS) 수행
 q = deque([x])
 while q:
     now = q.popleft()
-    # 이동할 수 있는 모든 도시를 확인
-    for next_node in adj[now]:
+    # 현재 도시에서 이동할 수 있는 모든 도시를 확인
+    for next_node in graph[now]:
         # 아직 방문하지 않은 도시라면
         if distance[next_node] == -1:
             # 최단 거리 갱신
