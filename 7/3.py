e37bca454e82b4611f443c623d39a420526404ac
Dongbin Na <ndb796@naver.com>
2020-06-29 09:53:23 +0900

    Update 3.py

--- 7/3.py
+++ 7/3.py
@@ -8,7 +8,7 @@ def binary_search(array, target, start, end):
         # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
         elif array[mid] > target:
             end = mid - 1
-        # 중간점의 값보다 찾고자 하는 값이 큰 같은 경우 오른쪽 확인
+        # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
         else:
             start = mid + 1
     return None
