d33052b2289e92a76e490b06f6ab0b1e2851aeff
Dongbin Na <ndb796@naver.com>
2020-06-29 10:06:54 +0900

    Update 1.py

--- 15/1.py
+++ 15/1.py
@@ -1,16 +1,16 @@
 from bisect import bisect_left, bisect_right
 
 # 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
-def count_by_range(a, left_value, right_value):
-    right_index = bisect_right(a, right_value)
-    left_index = bisect_left(a, left_value)
+def count_by_range(array, left_value, right_value):
+    right_index = bisect_right(array, right_value)
+    left_index = bisect_left(array, left_value)
     return right_index - left_index
 
 n, x = map(int, input().split()) # 데이터의 개수 N, 찾고자 하는 값 x 입력 받기
-data = list(map(int, input().split())) # 전체 데이터 입력 받기
+array = list(map(int, input().split())) # 전체 데이터 입력 받기
 
 # 값이 [x, x] 범위에 있는 데이터의 개수 계산
-count = count_by_range(data, x, x)
+count = count_by_range(array, x, x)
 
 # 값이 x인 원소가 존재하지 않는다면
 if count == 0:
