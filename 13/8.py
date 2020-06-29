ccbbfb5a1365140efd15e69b0f333ec605b1d663
Dongbin Na <ndb796@naver.com>
2020-06-29 14:48:18 +0900

    Update 8.py

--- 13/8.py
+++ 13/8.py
@@ -2,14 +2,14 @@ from collections import deque
 
 def get_next_pos(pos, board):
     next_pos = [] # 반환 결과 (이동 가능한 위치들)
-    pos = list(pos) # 현재 위치
+    pos = list(pos) # 현재 위치 정보를 리스트로 변환 (집합 → 리스트)
     pos1_x, pos1_y, pos2_x, pos2_y = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
     # (상, 하, 좌, 우)로 이동하는 경우에 대해서 처리
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     for i in range(4):
         pos1_next_x, pos1_next_y, pos2_next_x, pos2_next_y = pos1_x + dx[i], pos1_y + dy[i], pos2_x + dx[i], pos2_y + dy[i]
-        # 이동하고자 하는 두 칸이 모두 비어있다면
+        # 이동하고자 하는 두 칸이 모두 비어 있다면
         if board[pos1_next_x][pos1_next_y] == 0 and board[pos2_next_x][pos2_next_y] == 0:
             next_pos.append({(pos1_next_x, pos1_next_y), (pos2_next_x, pos2_next_y)})
     # 현재 로봇이 가로로 놓여 있는 경우
