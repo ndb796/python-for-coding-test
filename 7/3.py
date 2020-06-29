57a9feb3d42199b15c78d65022b7aff3a2ad16e9
Dongbin Na <ndb796@naver.com>
2020-06-29 09:50:39 +0900

    Update 3.py

--- 7/3.py
+++ 7/3.py
@@ -8,12 +8,12 @@ def binary_search(array, target, start, end):
         # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
         elif array[mid] > target:
             end = mid - 1
-        # 중간점의 값보다 찾고자 하는 값이 작은 경우 오른쪽 확인
+        # 중간점의 값보다 찾고자 하는 값이 크거나 같은 경우 오른쪽 확인
         else:
             start = mid + 1
     return None
 
-# n(원소의 개수)과 target(찾고자 하는 문자열)을 입력 받기
+# n(원소의 개수)과 target(찾고자 하는 값)을 입력 받기
 n, target = list(map(int, input().split()))
 # 전체 원소 입력 받기
 array = list(map(int, input().split()))
