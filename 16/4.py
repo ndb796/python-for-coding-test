cea27b7a36d0e054a444024adf0f8ca32c75a954
Dongbin Na <ndb796@naver.com>
2020-06-29 12:50:25 +0900

    Update 4.py

--- 16/4.py
+++ 16/4.py
@@ -1,13 +1,15 @@
 n = int(input())
-data = list(map(int, input().split()))
-# 순서를 바꾸어 '최장 증가 부분 수열' 문제로 변환
-data.reverse()
+array = list(map(int, input().split()))
+# 순서를 뒤집어 '최장 증가 부분 수열' 문제로 변환
+array.reverse()
 
-# LCS 알고리즘 수행
+# 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
 dp = [1] * n
+
+# 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
 for i in range(1, n):
     for j in range(0, i):
-        if data[j] < data[i]:
+        if array[j] < array[i]:
             dp[i] = max(dp[i], dp[j] + 1)
 
 # 열외해야 하는 병사의 최소 수를 출력
