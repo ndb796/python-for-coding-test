1b41801d87f39ad9bc6aac2bf6b51599aa77974f
Dongbin Na <ndb796@naver.com>
2020-06-29 11:57:28 +0900

    Update 2.py

--- 16/2.py
+++ 16/2.py
@@ -1,8 +1,8 @@
 n = int(input())
-data = []
+dp = []
 
 for _ in range(n):
-    data.append(list(map(int, input().split())))
+    dp.append(list(map(int, input().split())))
 
 # 다이나믹 프로그래밍으로 2번째 줄부터 내려가면서 확인
 for i in range(1, n):
@@ -11,13 +11,13 @@ for i in range(1, n):
         if j == 0:
             up_left = 0
         else:
-            up_left = data[i - 1][j - 1]
+            up_left = dp[i - 1][j - 1]
         # 바로 위에서 내려오는 경우
         if j == i:
             up = 0
         else:
             up = data[i - 1][j]
         # 최대 합을 저장
-        data[i][j] = data[i][j] + max(up_left, up)
+        dp[i][j] = dp[i][j] + max(up_left, up)
 
 print(max(data[n - 1]))
