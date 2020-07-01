f1cd2710c51d3179bf073974e3c7c3c12acc9bda
Dongbin Na <ndb796@naver.com>
2020-07-01 16:00:26 +0900

    Update 5.py

--- 10/5.py
+++ 10/5.py
@@ -5,12 +5,12 @@ v, e = map(int, input().split())
 # 모든 노드에 대한 진입차수는 0으로 초기화
 indegree = [0] * (v + 1)
 # 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트 초기화
-adj = [[] for i in range(v + 1)]
+graph = [[] for i in range(v + 1)]
 
 # 방향 그래프의 모든 간선 정보를 입력 받기
 for _ in range(e):
    a, b = map(int, input().split())
-   adj[a].append(b)
+   graph[a].append(b)
    # 진입 차수를 1 증가
    indegree[b] += 1
 
@@ -29,7 +29,7 @@ def topology_sort():
        now = q.popleft()
        result.append(now)
        # 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
-       for i in adj[now]:
+       for i in graph[now]:
            indegree[i] -= 1
            # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
            if indegree[i] == 0:
