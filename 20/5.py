48235c8b0977cd572d4d074d6a15db4de5028549
Dongbin Na <ndb796@naver.com>
2020-07-02 10:35:06 +0900

    Create 5.py

---
+++ 20/5.py
@@ -0,0 +1,15 @@
+# 데이터의 개수 N과 전체 데이터 선언
+n = 5
+data = [10, 20, 30, 40, 50]
+
+# 접두사 합(Prefix Sum) 배열 계산
+sum_value = 0
+prefix_sum = [0]
+for i in data:
+    sum_value += i
+    prefix_sum.append(sum_value)
+
+# 구간 합 계산 (세 번째 수부터 네 번째 수까지)
+left = 3
+right = 4
+print(prefix_sum[right] - prefix_sum[left - 1])
