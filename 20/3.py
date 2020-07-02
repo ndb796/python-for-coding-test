bc28acd072efaf3fb1b3e519d0f15327604d6163
Dongbin Na <ndb796@naver.com>
2020-07-02 10:13:36 +0900

    Create 3.py

---
+++ 20/3.py
@@ -0,0 +1,20 @@
+n = 5 # 데이터의 개수 N
+m = 5 # 찾고자 하는 부분합 M
+data = [1, 2, 3, 2, 5] # 전체 수열
+
+count = 0
+interval_sum = 0
+end = 0
+
+# start를 차례대로 증가시키며 반복
+for start in range(n):
+    # end를 가능한 만큼 이동시키기
+    while interval_sum < m and end < n:
+        interval_sum += data[end]
+        end += 1
+    # 부분합이 m일 때 카운트 증가
+    if interval_sum == m:
+        count += 1
+    interval_sum -= data[start]
+
+print(count)
