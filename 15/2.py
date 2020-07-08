6cdeda7c0f664b8f2144f7161a56c79f4a219ae8
Dongbin Na <ndb796@naver.com>
2020-07-09 06:26:04 +0900

    Update 2.py

--- 15/2.py
+++ 15/2.py
@@ -6,10 +6,10 @@ def binary_search(array, start, end):
     # 고정점을 찾은 경우 인덱스 반환
     if array[mid] == mid:
         return mid
-    # 중간점의 값보다 중간점이 작은 경우 왼쪽 확인
+    # 중간점이 가리키는 값보다 중간점이 작은 경우 왼쪽 확인
     elif array[mid] > mid:
         return binary_search(array, start, mid - 1)
-    # 중간점의 값보다 중간점이 큰 경우 오른쪽 확인
+    # 중간점이 가리키는 값보다 중간점이 큰 경우 오른쪽 확인
     else:
         return binary_search(array, mid + 1, end)
 
