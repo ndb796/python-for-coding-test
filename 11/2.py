b87186c7cd294fdf41315152fd4a0491a069c3b6
Dongbin Na <ndb796@naver.com>
2020-07-12 05:06:57 +0900

    Update 2.py

--- 11/2.py
+++ 11/2.py
@@ -1,13 +1,14 @@
-str = input()
+data = input()
 
 # 첫 번째 문자를 숫자로 변경하여 대입
-result = int(str[0])
+result = int(data[0])
 
-for i in range(1, len(str)):
+for i in range(1, len(data)):
     # 두 수 중에서 하나라도 '0' 혹은 '1'인 경우, 곱하기보다는 더하기 수행
-    if str[i] == '0' or str[i] == '1' or result <= 1:
-        result += int(str[i])
+    num = int(data[i])
+    if num <= 1 or result <= 1:
+        result += num
     else:
-        result *= int(str[i])
+        result *= num
 
 print(result)
