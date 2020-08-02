0766877e342365debab1e53de8a5f2c5c7fda7ff
Dongbin Na <ndb796@naver.com>
2020-08-03 01:20:48 +0900

    Update 1.py

--- 4/1.py
+++ 4/1.py
@@ -1,4 +1,4 @@
-# N 입력 받기
+# N 입력받기
 n = int(input())
 x, y = 1, 1
 plans = input().split()
@@ -8,7 +8,7 @@ dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 move_types = ['L', 'R', 'U', 'D']
 
-# 이동 계획을 하나씩 확인하기
+# 이동 계획을 하나씩 확인
 for plan in plans:
     # 이동 후 좌표 구하기
     for i in range(len(move_types)):
