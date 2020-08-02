8cb655632d00766ed5b5519c8b197ed6aafb58e5
Dongbin Na <ndb796@naver.com>
2020-08-03 01:07:24 +0900

    Update 1.java

--- 3/1.java
+++ 3/1.java
@@ -3,10 +3,10 @@ public class Main {
     public static void main(String[] args) {
         int n = 1260;
         int cnt = 0;
-        int[] types = {500, 100, 50, 10};
+        int[] coinTypes = {500, 100, 50, 10};
 		
         for (int i = 0; i < 4; i++) {
-            int coin = types[i];
+            int coin = coinTypes[i];
             cnt += n / coin;
             n %= coin;
         }
