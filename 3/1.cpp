4ff23bf70dcf771ad4eb9a81f3c01aeb23165508
Dongbin Na <ndb796@naver.com>
2020-08-03 01:07:05 +0900

    Update 1.cpp

--- 3/1.cpp
+++ 3/1.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int n = 1260;
 int cnt = 0;
-int types[4] = {500, 100, 50, 10};
+int coinTypes[4] = {500, 100, 50, 10};
 
 int main() {
     for (int i = 0; i < 4; i++) {
-        int coin = types[i];
+        int coin = coinTypes[i];
         cnt += n / coin;
         n %= coin;
     }
