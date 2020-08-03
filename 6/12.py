c21151fc645f88425161ddcfd5f326c5b95c4648
Dongbin Na <ndb796@naver.com>
2020-08-04 03:22:14 +0900

    Update 12.py

--- 6/12.py
+++ 6/12.py
@@ -1,15 +1,17 @@
-# N 입력 받기
-n = int(input())
+n, k = map(int, input().split()) # N과 K를 입력 받기
+a = list(map(int, input().split())) # 배열 A의 모든 원소를 입력받기
+b = list(map(int, input().split())) # 배열 B의 모든 원소를 입력받기
 
-count = [0] * 10001
-result = -1 # 가장 많이 가지고 있는 신발 번호
-max_value = 0 # 가장 많이 가지고 있는 신발 번호의 신발 개수
+a.sort() # 배열 A는 오름차순 정렬 수행
+b.sort(reverse=True) # 배열 B는 내림차순 정렬 수행
 
-for i in range(n):
-    a = int(input())
-    count[a] += 1
-    if max_value < count[a]:
-        max_value = count[a]
-        result = a # 가장 많이 가지고 있는 신발 번호 기록
+# 첫 번째 인덱스부터 확인하며, 두 배열의 원소를 최대 K번 비교
+for i in range(k):
+    # A의 원소가 B의 원소보다 작은 경우
+    if a[i] < b[i]:
+        # 두 원소를 교체
+        a[i], b[i] = b[i], a[i]
+    else: # A의 원소가 B의 원소보다 크거나 같을 때, 반복문을 탈출
+        break
 
-print(result)
+print(sum(a)) # 배열 A의 모든 원소의 합을 출력
