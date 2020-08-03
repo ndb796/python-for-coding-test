3cc55e5fc344f430ef27396efeba23c8bbbd4c6e
Dongbin Na <ndb796@naver.com>
2020-08-04 05:59:27 +0900

    Update 3.py

---
+++ 8/3.py
@@ -0,0 +1,12 @@
+d = [0] * 100
+
+def fibo(x):
+    print('f(' + str(x) + ')', end=' ')
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+print(fibo(6))
