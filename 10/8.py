6a169f4338cd2911f6f37339af1993ae77420d32
Dongbin Na <ndb796@naver.com>
2020-08-04 07:46:35 +0900

    Update 8.py

--- 10/8.py
+++ 10/8.py
@@ -14,9 +14,9 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-# 노드의 개수와 간선(Union 연산)의 개수 입력 받기
+# 노드의 개수와 간선(Union 연산)의 개수 입력받기
 v, e = map(int, input().split())
-parent = [0] * (v + 1) # 부모 테이블 초기화하기
+parent = [0] * (v + 1) # 부모 테이블 초기화
 
 # 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
 edges = []
@@ -26,7 +26,7 @@ result = 0
 for i in range(1, v + 1):
     parent[i] = i
 
-# 모든 간선에 대한 정보를 입력 받기
+# 모든 간선에 대한 정보를 입력받기
 for _ in range(e):
     a, b, cost = map(int, input().split())
     # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
