48663043af587d9eb3379a1f483d630c5a21d8d5
Dongbin Na <ndb796@naver.com>
2020-08-03 15:22:35 +0900

    Update 5.py

--- 5/5.py
+++ 5/5.py
@@ -8,11 +8,11 @@ def factorial_iterative(n):
 
 # 재귀적으로 구현한 n!
 def factorial_recursive(n):        
-    if n == 1:
+    if n <= 1: # n이 1 이하인 경우 1을 반환
         return 1
     # n! = n * (n - 1)!를 그대로 코드로 작성하기
     return n * factorial_recursive(n - 1)
 
-# 각각의 방식으로 구현한 n! 출력 (n = 5)
+# 각각의 방식으로 구현한 n! 출력(n = 5)
 print('반복적으로 구현:', factorial_iterative(5))
 print('재귀적으로 구현:', factorial_recursive(5))
