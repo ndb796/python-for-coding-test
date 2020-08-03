bbe9e126c0437792386b8ac2cb92e6a1df698f6f
Dongbin Na <ndb796@naver.com>
2020-08-04 06:13:10 +0900

    Update 8.py

--- 8/8.py
+++ 8/8.py
@@ -12,11 +12,11 @@ d = [10001] * (m + 1)
 d[0] = 0
 for i in range(n):
     for j in range(array[i], m + 1):
-        if d[j - array[i]] != 10001: # (i - k) 원을 만드는 방법이 존재하는 경우
+        if d[j - array[i]] != 10001: # (i - k)원을 만드는 방법이 존재하는 경우
             d[j] = min(d[j], d[j - array[i]] + 1)
 
 # 계산된 결과 출력
-if d[m] == 10001: # 최종적으로 m 원을 만드는 방법이 없는 경우
+if d[m] == 10001: # 최종적으로 M원을 만드는 방법이 없는 경우
     print(-1)
 else:
     print(d[m])
