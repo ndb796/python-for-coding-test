4d7dca73334eec0413bdf869fe2efcc9122a83fd
Dongbin Na <ndb796@naver.com>
2020-08-04 06:08:45 +0900

    Update 8.py

--- 8/8.py
+++ 8/8.py
@@ -8,7 +8,7 @@ for i in range(n):
 # 한 번 계산된 결과를 저장하기 위한 DP 테이블 초기화
 d = [10001] * (m + 1)
 
-# 다이나믹 프로그래밍(Dynamic Programming) 진행 (보텀업)
+# 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
 d[0] = 0
 for i in range(n):
     for j in range(array[i], m + 1):
