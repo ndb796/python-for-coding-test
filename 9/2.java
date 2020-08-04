103f9e9075154dac69b81f60063bbb9059a58a68
ndb796 <ndb796@naver.com>
2020-08-04 21:38:06 +0900

    Update

---
+++ 9/2.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+class Node implements Comparable<Node> {
+
+    private int index;
+    private int distance;
+
+    public Node(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+
+    // 거리(비용)가 짧은 것이 높은 우선순위를 가지도록 설정
+    @Override
+    public int compareTo(Node other) {
+        if (this.distance < other.distance) {
+            return -1;
+        }
+        return 1;
+    }
+}
+
+
+public class Main {
+
+    public static final int INF = (int) 1e9; // 무한을 의미하는 값으로 10억을 설정
+    // 노드의 개수(N), 간선의 개수(M), 시작 노드 번호(Start)
+    // 노드의 개수는 최대 100,000개라고 가정
+    public static int n, m, start;
+    // 각 노드에 연결되어 있는 노드에 대한 정보를 담는 배열
+    public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+    // 최단 거리 테이블 만들기
+    public static int[] d = new int[100001];
+
+    public static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        // 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
+        pq.offer(new Node(start, 0));
+        d[start] = 0;
+        while(!pq.isEmpty()) { // 큐가 비어있지 않다면
+            // 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+            Node node = pq.poll();
+            int dist = node.getDistance(); // 현재 노드까지의 비용 
+            int now = node.getIndex(); // 현재 노드
+            // 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+            if (d[now] < dist) continue;
+            // 현재 노드와 연결된 다른 인접한 노드들을 확인
+            for (int i = 0; i < graph.get(now).size(); i++) {
+                int cost = d[now] + graph.get(now).get(i).getDistance();
+                // 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+                if (cost < d[graph.get(now).get(i).getIndex()]) {
+                    d[graph.get(now).get(i).getIndex()] = cost;
+                    pq.offer(new Node(graph.get(now).get(i).getIndex(), cost));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        start = sc.nextInt();
+
+        // 그래프 초기화
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Node>());
+        }
+        
+        // 모든 간선 정보를 입력받기
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            // a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+            graph.get(a).add(new Node(b, c));
+        }
+
+        // 최단 거리 테이블을 모두 무한으로 초기화
+        Arrays.fill(d, INF);
+        
+        // 다익스트라 알고리즘을 수행
+        dijkstra(start);
+
+        // 모든 노드로 가기 위한 최단 거리를 출력
+        for (int i = 1; i <= n; i++) {
+            // 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+            if (d[i] == INF) {
+                System.out.println("INFINITY");
+            }
+            // 도달할 수 있는 경우 거리를 출력
+            else {
+                System.out.println(d[i]);
+            }
+        }
+    }
+}
---
+++ 9/4.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Main {
+
+    public static final int INF = (int) 1e9; // 무한을 의미하는 값으로 10억을 설정
+    // 노드의 개수(N), 간선의 개수(M), 거쳐 갈 노드(X), 최종 목적지 노드(K)
+    public static int n, m, x, k;
+    // 2차원 배열(그래프 표현)를 만들기
+    public static int[][] graph = new int[101][101];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        // 최단 거리 테이블을 모두 무한으로 초기화
+        for (int i = 0; i < 101; i++) {
+            Arrays.fill(graph[i], INF);
+        }
+
+        // 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                if (a == b) graph[a][b] = 0;
+            }
+        }
+
+        // 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
+        for (int i = 0; i < m; i++) {
+            // A와 B가 서로에게 가는 비용은 1이라고 설정
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a][b] = 1;
+            graph[b][a] = 1;
+        }
+
+        x = sc.nextInt();
+        k = sc.nextInt();
+
+        // 점화식에 따라 플로이드 워셜 알고리즘을 수행
+        for (int k = 1; k <= n; k++) {
+            for (int a = 1; a <= n; a++) {
+                for (int b = 1; b <= n; b++) {
+                    graph[a][b] = Math.min(graph[a][b], graph[a][k] + graph[k][b]);
+                }
+            }
+        }
+
+        // 수행된 결과를 출력
+        int distance = graph[1][k] + graph[k][x];
+
+        // 도달할 수 없는 경우, -1을 출력
+        if (distance >= INF) {
+            System.out.println(-1);
+        }
+        // 도달할 수 있다면, 최단 거리를 출력
+        else {
+            System.out.println(distance);
+        }
+    }
+}
