0c1bf56f329455861f6eaf0f14fc7d02518b250f
Dongbin Na <ndb796@naver.com>
2020-08-05 17:12:23 +0900

    Update 6.py

--- 12/6.py
+++ 12/6.py
@@ -7,7 +7,7 @@ def possible(answer):
                 continue
             return False # 아니라면 거짓(False) 반환
         elif stuff == 1: # 설치된 것이 '보'인 경우
-            # '한쪽 끝 부분이 기둥 위' 혹은 '양쪽 끝 부분이 다른 보와 동시에 연결'이라면 정상
+            # '한쪽 끝부분이 기둥 위' 혹은 '양쪽 끝부분이 다른 보와 동시에 연결'이라면 정상
             if [x, y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or ([x - 1, y, 1] in answer and [x + 1, y, 1] in answer):
                 continue
             return False # 아니라면 거짓(False) 반환
@@ -22,7 +22,7 @@ def solution(n, build_frame):
             if not possible(answer): # 가능한 구조물인지 확인
                 answer.append([x, y, stuff]) # 가능한 구조물이 아니라면 다시 설치
         if operate == 1: # 설치하는 경우
-            answer.append([x, y, stuff]) # 일단 설치를 해 본 뒤에
+            answer.append([x, y, stuff]) # 일단 설치를 해본 뒤에
             if not possible(answer): # 가능한 구조물인지 확인
                 answer.remove([x, y, stuff]) # 가능한 구조물이 아니라면 다시 제거
     return sorted(answer) # 정렬된 결과를 반환
