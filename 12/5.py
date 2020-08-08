e6fed3e5c27da4bec5a2bca40d02ceddba20a8ab
Dongbin Na <ndb796@naver.com>
2020-08-09 01:17:32 +0900

    Update 5.py

--- 12/5.py
+++ 12/5.py
@@ -3,7 +3,7 @@ k = int(input())
 data = [[0] * (n + 1) for _ in range(n + 1)] # 맵 정보
 info = [] # 방향 회전 정보
 
-# 맵 정보 (사과 있는 곳은 1로 표시)
+# 맵 정보(사과 있는 곳은 1로 표시)
 for _ in range(k):
     a, b = map(int, input().split())
     data[a][b] = 1
@@ -14,7 +14,7 @@ for _ in range(l):
     x, c = input().split()
     info.append((int(x), c))
 
-# 처음에는 오른쪽을 보고 있으므로 (동, 남, 서, 북)
+# 처음에는 오른쪽을 보고 있으므로(동, 남, 서, 북)
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
