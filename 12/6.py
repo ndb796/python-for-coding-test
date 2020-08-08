88c9c6469561382fbc5a4a5b6b8e5ea6e1060c48
Dongbin Na <ndb796@naver.com>
2020-08-09 01:19:07 +0900

    Update 6.py

--- 12/6.py
+++ 12/6.py
@@ -2,7 +2,7 @@
 def possible(answer):
     for x, y, stuff in answer:
         if stuff == 0: # 설치된 것이 '기둥'인 경우
-            # '바닥 위' 혹은 '보의 한 쪽 끝 부분 위' 혹은 '다른 기둥 위'라면 정상
+            # '바닥 위' 혹은 '보의 한쪽 끝 부분 위' 혹은 '다른 기둥 위'라면 정상
             if y == 0 or [x - 1, y, 1] in answer or [x, y, 1] in answer or [x, y - 1, 0] in answer:
                 continue
             return False # 아니라면 거짓(False) 반환
@@ -18,7 +18,7 @@ def solution(n, build_frame):
     for frame in build_frame: # 작업(frame)의 개수는 최대 1,000개
         x, y, stuff, operate = frame
         if operate == 0: # 삭제하는 경우
-            answer.remove([x, y, stuff]) # 일단 삭제를 해 본 뒤에
+            answer.remove([x, y, stuff]) # 일단 삭제를 해본 뒤에
             if not possible(answer): # 가능한 구조물인지 확인
                 answer.append([x, y, stuff]) # 가능한 구조물이 아니라면 다시 설치
         if operate == 1: # 설치하는 경우
