881e07d3ecbd4ebb2b6fa62b4427032b30551a29
Dongbin Na <ndb796@naver.com>
2020-08-09 23:41:38 +0900

    Update 1.py

--- 18/1.py
+++ 18/1.py
@@ -14,9 +14,9 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-# 여행지의 개수와 여행 계획에 속한 여행지의 개수 입력 받기
+# 여행지의 개수와 여행 계획에 속한 여행지의 개수 입력받기
 n, m = map(int, input().split())
-parent = [0] * (n + 1) # 부모 테이블 초기화하기
+parent = [0] * (n + 1) # 부모 테이블 초기화
 
 # 부모 테이블상에서, 부모를 자기 자신으로 초기화
 for i in range(1, n + 1):
@@ -29,7 +29,7 @@ for i in range(n):
         if data[j] == 1: # 연결된 경우 합집합(Union) 연산 수행
             union_parent(parent, i + 1, j + 1)
 
-# 여행 계획 입력 받기
+# 여행 계획 입력받기
 plan = list(map(int, input().split()))
 
 result = True
