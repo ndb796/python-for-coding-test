1a7d405fa8f06e2d578b0e5b4ddb866eb819dbf5
Dongbin Na <ndb796@naver.com>
2020-08-12 08:53:39 +0900

    Update 1.py

--- 3/1.py
+++ 3/1.py
@@ -2,9 +2,9 @@ n = 1260
 count = 0
 
 # 큰 단위의 화폐부터 차례대로 확인하기
-list = [500, 100, 50, 10]
+coin_types = [500, 100, 50, 10]
 
-for coin in list:
+for coin in coin_types:
     count += n // coin # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
     n %= coin
 
