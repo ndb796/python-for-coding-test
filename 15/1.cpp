1cfd39ae86b473bd1684961b1719a1bde04567ad
Dongbin Na <ndb796@naver.com>
2020-09-02 14:49:17 +0900

    Update 1.cpp

--- 15/1.cpp
+++ 15/1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
-int countByRange(vector<int> v, int leftValue, int rightValue) {
+int countByRange(vector<int>& v, int leftValue, int rightValue) {
     vector<int>::iterator rightIndex = upper_bound(v.begin(), v.end(), rightValue);
     vector<int>::iterator leftIndex = lower_bound(v.begin(), v.end(), leftValue);
     return rightIndex - leftIndex;
