3a1279cd52b461419455a0dff7448cfc4e169cac
Dongbin Na <ndb796@naver.com>
2020-09-05 15:08:21 +0900

    Update 6.py

--- 10/6.py
+++ 10/6.py
@@ -18,10 +18,12 @@ for _ in range(e):
 def topology_sort():
     result = [] # 알고리즘 수행 결과를 담을 리스트
     q = deque() # 큐 기능을 위한 deque 라이브러리 사용
+
     # 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
     for i in range(1, v + 1):
         if indegree[i] == 0:
             q.append(i)
+
     # 큐가 빌 때까지 반복
     while q:
         # 큐에서 원소 꺼내기
@@ -33,6 +35,7 @@ def topology_sort():
             # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
             if indegree[i] == 0:
                 q.append(i)
+
     # 위상 정렬을 수행한 결과 출력
     for i in result:
         print(i, end=' ')
