a5889fa98dec929048a97a6b35e32a1a2335679f
Ryan Jung <jjs1005k@naver.com>
2020-09-09 17:41:56 +0900

    Edit indent

--- 4/1.py
+++ 4/1.py
@@ -15,10 +15,11 @@ for plan in plans:
         if plan == move_types[i]:
             nx = x + dx[i]
             ny = y + dy[i]
-    # 공간을 벗어나는 경우 무시
-    if nx < 1 or ny < 1 or nx > n or ny > n:
-        continue
-    # 이동 수행
-    x, y = nx, ny
+            
+            # 공간을 벗어나는 경우 무시
+            if nx < 1 or ny < 1 or nx > n or ny > n:
+                continue
+            # 이동 수행
+            x, y = nx, ny
 
 print(x, y)
